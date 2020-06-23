15ffc0607ee015805ade833675c444f453938f12
Ivar Conradi Østhus <ivarconr@gmail.com>
2020-06-23 22:06:19 +0200

    inital structure

--- src/main/java/com/unleashhosted/example/unleashdemo/HelloController.java
+++ src/main/java/com/unleashhosted/example/unleashdemo/HelloController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpSession;
 
 import no.finn.unleash.Unleash;
 import no.finn.unleash.UnleashContext;
+import no.finn.unleash.Variant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,9 +23,20 @@ public class HelloController {
 
         boolean isEnabled = unleash.isEnabled("Demo", context);
         if(unleash.isEnabled("Demo")) {
-            return "Greeting, feature flag is enabled";
+            return "Greeting, Demo flag is enabled";
         } else {
-            return "Doh, it was disabled.";
+            return "Doh, Demo was disabled.";
         }
+    }
+
+
+    @RequestMapping("/variants")
+    public String variants(HttpSession session) {
+        UnleashContext context = UnleashContext.builder()
+                .sessionId(session.getId())
+                .build();
+
+        Variant variant = unleash.getVariant("VariantExample", context);
+        return "variant selected: " + variant.getName();
     }
 }
--- src/main/java/com/unleashhosted/example/unleashdemo/UnleashDemoApplication.java
+++ src/main/java/com/unleashhosted/example/unleashdemo/UnleashDemoApplication.java
@@ -1,8 +1,5 @@
 package com.unleashhosted.example.unleashdemo;
 
-import no.finn.unleash.DefaultUnleash;
-import no.finn.unleash.Unleash;
-import no.finn.unleash.util.UnleashConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
